5ce9fae2182d7709ddf87e7bc9fe49813cacef95
CttIsmael <ismaeloliveirarego80@gmail.com>
2020-09-14 11:33:14 -0300

    Quiz

---
+++ Desafio.py
@@ -0,0 +1,23 @@
+print("No Python, como se chama uma 'caixa' usada para armazenar dados?")
+
+score = 0
+
+respostas = input().lower()
+
+if respostas == "variavel":
+    score = score + 1
+    print("Acertou")
+else:
+    print("Errou")
+
+print("Nome da operação que cria instruções nativas (built-ins) para armazenar texto?")
+
+respostas = input().lower()
+
+if respostas == "string":
+    score = score + 1
+    print("Acertou")
+else:
+    print("Errou ")
+
+print(f"Obrigado por jogar! pontuação: {score}")
